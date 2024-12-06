c8e4f43aad0e6dbb6de4174a47809bd9cedcb0e8
madlnu <madlnu@visa.com>
2024-12-07 00:49:34 +0530

    Initial data population

--- fetch_pr.py
+++ fetch_pr.py
@@ -1,14 +1,15 @@
 import requests
+import json
 
 username = "Madhukar525722"
 token = "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"
 
-url = f"https://api.github.com/search/issues?q=author:{username}+type:pr"
+url = "https://api.github.com/search/issues?q=author:{}+type:pr".format(username)
 headers = {
-    "Authorization": f"token {token}"
+    "Authorization": "token {}".format(token)
 }
 
-response = requests.get(url, headers=headers)
+response = requests.get(url, headers=headers, verify=False)
 data = response.json()
 
 with open('pr_data.json', 'w') as f:
---
+++ instructions.md
@@ -0,0 +1,4 @@
+How to run update:
+1. Run the script: python3 fetch_pr.py
+2. Add the generated json file
+3. Push the changes to git
